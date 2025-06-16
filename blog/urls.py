433558d05ebfbf3a8df64d30e123147cc0405bff
divyesh <rajjivani027@gmail.com>
2025-06-16 15:50:34 +0530

    Edited base.html for header and main part also added css

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('home/',home,name="blog-home"),
     path('about/',about,name="blog-about"),
-]
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
