b7e1c3e97e23ac74aab7a2463e6fed8a95375fa9
divyesh <rajjivani027@gmail.com>
2025-06-17 22:43:33 +0530

    Added Posts model and updated UserManager and CustomUser Model

--- blog/manager.py
+++ blog/manager.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,phone_number,password = None,**extra_fields):
+    def create_user(self,email,phone_number,password,**extra_fields):
         if not email or not phone_number:
             s = ""
             if not email and not phone_number:
@@ -13,13 +13,13 @@ class UserManager(BaseUserManager):
             raise ValueError(s)
 
         email = self.normalize_email(email)
-        user = self.model(phone_number=phone_number,**extra_fields)
+        user = self.model(email=email,phone_number=phone_number,**extra_fields)
         user.set_password(password)
         user.save(using = self._db)
 
         return user
     
-    def create_superuser(self,email,phone_number,password = None,**extra_fields):
+    def create_superuser(self,email,phone_number,password,**extra_fields):
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_superuser',True)
         extra_fields.setdefault('is_active',True)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .manager import UserManager
+from django.utils import timezone
 # Create your models here.
 
 class CustomUser(AbstractUser):
@@ -8,10 +9,20 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=30,unique=True)
     profile_pic = models.ImageField()
     user_bio = models.CharField(max_length=50)
+    dob = models.DateField(default="2000-01-01")
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['phone_number']
+    REQUIRED_FIELDS = ['phone_number','username','dob']
+
+    def __str__(self):
+        return self.username
 
 class Posts(models.Model):
-    heading = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
