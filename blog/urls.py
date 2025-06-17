a8e84df18d71089be41f79b022d288d0890a6867
divyesh <rajjivani027@gmail.com>
2025-06-17 22:41:41 +0530

    Added Register View

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path('home/',home,name="blog-home"),
     path('about/',about,name="blog-about"),
+    path('register/',register,name="blog-register")
 ] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Raj Jivani',
-        'title':'Blog Post 1',
-        'content':'First Blog Content',
-        'date_posted':'June 12,2025'
-    },
-    {
-        'author':'Divyesh Jivani',
-        'title':'Blog Post 2',
-        'content':'Second Blog Content',
-        'date_posted':'June 11,2025'
-    }
-]
+from .models import Posts
 
 def home(request):
+    posts = Posts.objects.all()
     context = {
-        'posts':posts,
-        'title':'Home'
+        'posts' : posts,
+        'title' : "Home"
     }
     return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
+
+def register(request):
+    return render(request,'blog/register.html',{'title' : 'Register'})
 
 # Create your views here.
