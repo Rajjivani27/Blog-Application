bed9f726649d521cfe666f544cb395f0df870aba
divyesh <rajjivani027@gmail.com>
2025-06-20 15:31:04 +0530

    Updated template and urls to use Django's in-built LoginView

--- blog/urls.py
+++ blog/urls.py
@@ -2,14 +2,15 @@ from django.urls import path,include
 from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('home/',home,name="blog-home"),
     path('about/',about,name="blog-about"),
-    path('login/',login_page,name="blog-login"),
+    path('login/',auth_views.LoginView.as_view(template_name = "blog/login.html"),name="blog-login"),
     path('register/',register_page,name="blog-register"),
-    path('logout/',logoutView,name="blog-logout")
+    path('logout/',logoutView,name="blog-logout"),
 ] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT) 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
