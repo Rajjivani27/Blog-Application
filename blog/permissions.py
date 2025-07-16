a3cfbae1babffa64344fa4054e654f843dc6698b
divyesh <rajjivani027@gmail.com>
2025-07-16 23:45:34 +0530

    Created Custom Permission IsAuthor

--- blog/permissions.py
+++ blog/permissions.py
@@ -12,3 +12,14 @@ class IsAuthorOrReadOnly(BasePermission):
             return True
 
         return obj.author == request.user
+    
+class IsAuthor(BasePermission):
+    """
+    Custom Permission:
+    - Checks for the person requested the action is author of post/comment or not
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if obj.author == request.user:
+            return True
+        return False
